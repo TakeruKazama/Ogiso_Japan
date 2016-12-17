4a382ba6683813da48b73f746c4b49099941627c
TakeruKazama <kzm_rhacodactylus@softbank.ne.jp>
2016-12-18 01:42:50 +0900

    画面表示

--- event/views.py
+++ event/views.py
@@ -1,3 +1,9 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.template import loader
 
-# Create your views here.
+
+def league(request):
+    template = loader.get_template('event/league.html')
+    context = {}
+    return HttpResponse(template.render(context, request))
